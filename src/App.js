38ff84abf7fc51ca723221a93f293e982a7d8c1d
tapiwa madzanise <tmadz8779@gmail.com>
2022-08-12 06:46:53 +0200

    added everything

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import './App.css';
+import Input from './components/Input';
+
+function App() {
+  return (
+    <div className="app">
+     <h1>hello clever programmer</h1>
+     <Input />
+    </div>
+  );
+}
+
+export default App;
